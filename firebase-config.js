2cbf259440184e23f3325f546a495c9621283bf0
Hendri040921 <rotusanto@gmail.com>
2025-05-27 01:25:32 +0700

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDm5uJ7QwE8XxYOUR_API_KEY_HERE",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdefghijk1234567890"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
